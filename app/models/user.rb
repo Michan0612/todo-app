72f49429e4804f8c00a0ac31accab641b303d11c
Tanaka Mitsuyuki <tomoka6412@gmail.com>
2020-06-20 08:56:54 +0900

    編集と削除画面を作成しました

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,6 +23,11 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :boards, dependent: :destroy
+
+  def has_written?(board)
+    boards.exists?(id: board.id)
+  end
+
   
   def avatar_image
       'default-avatar.png'
