91123f6cb2f2e34f315a8191813af48856060fdd
Tanaka Mitsuyuki <tomoka6412@gmail.com>
2020-06-21 08:39:06 +0900

    taskの編集・削除とアイキャッチ画像の設定をしました

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,33 +1,63 @@
 class TasksController < ApplicationController
 		before_action :set_article, only: %i[show]
 
-		def show
 
+		def index
+				@tasks = Task.all
+		end
+
+		def show
+				
 		end		
 
 		def new
         board = Board.find(params[:board_id])
-        @task = board.tasks.build
+				@task = board.tasks.build
+				@task.user = current_user
     end
   
     def create
-        board = Board.find(params[:board_id])
-        @task = board.tasks.build(task_params)
+        board = Board.find(params[:board_id]) 
+				@task = board.tasks.build(task_params)
+				@task.user = current_user
         if @task.save
             redirect_to board_path(board), notice: 'コメントを追加'
         else
             flash.now[:error] = '更新できませんでした'
             render :new
         end
-    end
+		end
+
+		def edit
+				board = Board.find(params[:board_id]) 
+				@task = current_user.tasks.find(params[:id])
+		end
+	
+		def update
+				@task = current_user.tasks.find(params[:id])
+				if @task.update(task_params)
+						redirect_to board_task_path(@task), notice: '更新できました'
+				else
+						flash.now[:error] = '更新できませんでした'
+						render :edit
+				end
+		end
+
+		def destroy
+			task = current_user.tasks.find(params[:id])
+			task.destroy!
+			redirect_to board_path(board), notice: '削除に成功しました'
+		end
+		
 
     private
     def task_params
-        params.require(:task).permit(:content, :title)
+        params.require(:task).permit(:content, :title, :eyecatch)
 		end
 
 		def set_article
-			@task = Task.find(params[:id])
+				@task = Task.find(params[:id])
 		end
-		
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -21,13 +21,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  
   has_many :boards, dependent: :destroy
+  has_many :tasks, dependent: :destroy
 
   def has_written?(board)
     boards.exists?(id: board.id)
   end
 
+  def has_written2?(task)
+    tasks.exists?(id: task.id)
+  end
   
   def avatar_image
       'default-avatar.png'
